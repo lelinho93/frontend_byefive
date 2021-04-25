cbd2ec6be7c40ca21d4871cb91a73fe9a05da534
wesley <lelinho93@gmail.com>
2021-04-25 17:30:19 -0300

    logout component

---
+++ byefive_front/src/components/Logout/Logout.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useHistory } from 'react-router';
+import { Button } from '@material-ui/core';
+import { ButtonsContainer } from './styles';
+import { Link } from 'react-router-dom';
+
+
+
+
+const Logout = () => {
+    const history = useHistory()
+
+    function logout() {
+        window.localStorage.removeItem('token')
+        history.push('/')
+    }
+
+
+  return <div>   
+    <ButtonsContainer>      
+      <Link>
+        <Button  variant={'contained'} color={'primary'} onClick={logout}>Sair do Sistema</Button>
+      </Link>
+    </ButtonsContainer>
+  </div>
+}
+
+export default Logout
