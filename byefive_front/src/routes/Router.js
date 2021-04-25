f6c0a4e50832af8619982551f475d19ac45b4ec2
wesley <lelinho93@gmail.com>
2021-04-25 14:43:20 -0300

    configs files and router update

--- byefive_front/src/routes/Router.js
+++ byefive_front/src/routes/Router.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Login from '../pages/LoginPage'
 
 
 export default function Router() {
@@ -7,7 +8,7 @@ export default function Router() {
         <BrowserRouter>
             <Switch>
                 <Route exact path= "/">
-
+                    <Login/>
                 </Route>
                 <Route exact path= "/signup">
 
